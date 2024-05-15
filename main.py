99066a40170d659e6e1da8967d409fc32ef6db49
harinbharadia <169069412+harinbharadia@users.noreply.github.com>
2024-05-15 16:22:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Rap Name generator yo!")
+hobby = input("What did you want to be when you were younger?\n")
+nickname = input("What was your childhood nickname?\n")
+print(" ")
+print("Your rap name is....\n")
+print(nickname + " Da " + hobby)
